a105735bde3c61b0269bdfc4d19d761e65dc9174
mbunge <marco.bunge@web.de>
2016-01-09 16:27:46 +0100

    setup config initiator

---
+++ tests/Config/InitiatorTest.php
@@ -0,0 +1,23 @@
+<?php
+/*
+*
+* (c) Marco Bunge <marco_bunge@web.de>
+*
+* For the full copyright and license information, please view the LICENSE.txt
+* file that was distributed with this source code.
+*
+* Date: 09.01.2016
+* Time: 16:26
+*/
+
+namespace Turbine\Tests\Config;
+
+
+class InitiatorTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function testHttpConfig()
+    {
+    }
+}
